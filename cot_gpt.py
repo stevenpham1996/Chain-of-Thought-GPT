2f40cb8f2840461709a5604f4eb23ace1983177b
Steven Pham <stevenpham1996@outlook.com>
2023-05-13 14:09:46 +0700

    Add files via upload

---
+++ cot_gpt.py
@@ -0,0 +1,74 @@
+import openai
+import os
+
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.getenv('OPENAI_API_KEY')
+
+def generate_response(messages, model="gpt-3.5-turbo", temperature=0.5): 
+    #messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        max_tokens=2000,
+        n=1,
+        stop=None,
+        temperature=temperature, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"].strip()
+
+def main(num_options=3):
+    while True:
+        print()
+        print("---------------------------------------------------------------")
+        print()
+        user_message = input("Ask CoT-GPT a question: ")
+        prefixed_message = f"Question: {user_message}.\n Answer: Let's work this out \
+        in a step by step way to be sure we have the right answer."
+        
+        answer_options = []
+        for i in range(num_options):
+            answer_option = generate_response([
+                {"role": "user", "content": prefixed_message},
+            ])
+            print()
+            print()
+            print(f"Answer option {i+1}:\n")
+            print(answer_option)
+            print()
+            print("---------------------------------------------------------------")
+            answer_options.append(f"Answer option number {i+1}: {answer_option}")
+        
+        researcher_prompt = f"Question: {prefixed_message}\n \
+        Answers: {answer_options}"
+        researcher_prompt += f"\n\nBased on the {num_options} answer options provided, \
+        you are tasked with investigating the flaws and faulty logics of each answer option.\
+        Let's work this out in a step by step way to be sure we indentify all the errors:"
+        researcher_response = generate_response([
+            {"role": "user", "content": researcher_prompt},
+        ])
+        print()
+        print()
+        print("Researcher's response:\n")
+        print(researcher_response)
+        print()
+        print("---------------------------------------------------------------")
+        
+        resolver_prompt = f"Question: {prefixed_message}\n \
+        Answers: {answer_options}\n \
+        Researcher's Response: {researcher_response}"
+        resolver_prompt += f"\n\n You are a resolver taksed with finding which of the {num_options} answer options \
+        the Reseacher thought was best, improving upon it and elaborate the improved answer in full.\
+        Let's work this out in a step by step way to be sure we indentify all the errors:"           
+        resolver_response = generate_response([
+            {"role": "user", "content": resolver_prompt},
+        ])
+        print()
+        print()
+        print("Resolver's response:\n")
+        print(resolver_response)
+        
+if __name__ == "__main__":
+    main()
---
+++ logic_gpt.py
@@ -0,0 +1,64 @@
+import openai
+import os
+
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.getenv('OPENAI_API_KEY')
+
+def generate_response(messages, model="gpt-3.5-turbo", temperature=0.5): 
+    #messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        max_tokens=2000,
+        n=1,
+        stop=None,
+        temperature=temperature, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"].strip()
+
+def main(num_options=2):  
+    while True:
+        print()
+        print("---------------------------------------------------------------")
+        print()
+        user_message = input("Ask LogicGPT a question: ")
+        prefixed_message = f"Question: {user_message}.\n Answer: Let's work this out \
+        in a step by step way to be sure we have the right answer."
+        
+        answer_options = []
+        for i in range(num_options):
+            answer_option = generate_response([
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prefixed_message},
+            ])
+            print()
+            print()
+            print(f"Answer option {i+1}:\n")
+            print(answer_option)
+            print()
+            print("---------------------------------------------------------------")
+            answer_options.append(f"Answer option number {i+1}: {answer_option}")
+        
+        prompt = f"Question: {prefixed_message}\n \
+        Answers: {answer_options}"
+        prompt += f"\n\nBased on the {num_options} answer options provided, \
+        as a thoughtful thinker, you are tasked to identify the one answer option \
+        whose chain of thoughts is the most logical or consistent, \
+        correct any flaws and faulty logic that may still exist, \
+        then discuss the one final improved answer in depth. \
+        Let's work this out in a step by step way to make sure we achieve our objective:"
+        response = generate_response([
+        {"role": "user", "content": prompt},
+        ])
+        print()
+        print()
+        print("LogicGPT's response:\n")
+        print(response)
+
+
+    
+if __name__ == "__main__":
+    main()
